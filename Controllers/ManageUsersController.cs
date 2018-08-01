6b5d26298fe672eb8e1465e4a2c78af78bda7c3b
Michael.Canova <1michaelcanova@gmail.com>
2018-07-31 20:05:51 -0500

    started adding admin role

---
+++ Controllers/ManageUsersController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ManageUsersController(UserManager<ApplicationUser> manager)
+        {
+            userManager = manager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var admins = (await userManager.GetUsersInRoleAsync("Administrator")).ToList();
+
+            var everyone = await userManager.Users.ToListAsync();
+
+            var model = new ManageUsersViewModel
+            {
+                Administrators = admins,
+                Everyone = everyone
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/ManageUsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreTodo.Models
+{
+    public class ManageUsersViewModel
+    {
+        public List<ApplicationUser> Administrators { get; set; }
+        public List<ApplicationUser> Everyone { get; set; }
+    }
+}
