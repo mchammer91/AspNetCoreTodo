f99476376ecf924deb2d50c5a49fff9ea3052f6d
Michael Canova <michael.canova@q2ebanking.com>
2018-09-17 14:46:59 -0500

    added UnitTests project outside of base project folder; created top folder to contain all projects

---
+++ AspNetCoreTodo.UnitTests/ToDoItemServiceShould.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class ToDoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new ToDoItemService(context);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new ToDoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context.Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?", item.Title);
+                Assert.False(item.IsDone);
+
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                Assert.True(difference < TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
