4e1cd4554a0bbb404aaf8804a82c8e5bb8736d12
Michael Canova <michael.canova@q2ebanking.com>
2018-09-17 15:18:40 -0500

    completed book -- now do some exploring!

---
+++ AspNetCoreTodo.IntegrationTests/TestFixure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TestFixure : IDisposable
+    {
+        private readonly TestServer testServer;
+
+        public HttpClient Client { get; }
+
+        public TestFixure()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<AspNetCoreTodo.Startup>().ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), @"../../../../AspNetCoreTodo"));
+
+                    config.AddJsonFile("appsettings.json");
+                });
+
+            testServer = new TestServer(builder);
+
+            Client = testServer.CreateClient();
+            Client.BaseAddress = new Uri(@"http:/localhost:8888");
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            testServer.Dispose();
+        }
+    }
+}
---
+++ AspNetCoreTodo.IntegrationTests/TodoRouteShould.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    class TodoRouteShould
+    {
+    }
+}
