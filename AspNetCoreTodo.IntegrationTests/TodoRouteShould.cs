79980bf925c0240dfbb121e62be2ef32158dc51c
Michael Canova <michael.canova@q2ebanking.com>
2018-09-17 15:19:08 -0500

    integration test

--- AspNetCoreTodo.IntegrationTests/TodoRouteShould.cs
+++ AspNetCoreTodo.IntegrationTests/TodoRouteShould.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace AspNetCoreTodo.IntegrationTests
 {
-    class TodoRouteShould
+    public class TodoRouteShould : IClassFixture<TestFixure>
     {
+        private readonly HttpClient httpClient;
+
+        public TodoRouteShould(TestFixure fixture)
+        {
+            httpClient = fixture.Client;
+        }
+
+        [Fact]
+        public async Task ChallengeAnonymousUser()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/todo");
+
+            var response = await httpClient.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal(@"http://localhost:8888/Account/Login?ReturnUrl=%2Ftodo", response.Headers.Location.ToString());
+        }
     }
 }
